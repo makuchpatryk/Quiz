5b0ce4f73c196a5e48e391f3ae1fc3a509064567
patryk makuch <makuchpatryk@gmail.com>
2018-07-28 18:35:35 +0100

    implement postgresql db

--- project_4/project_4/settings.py
+++ project_4/project_4/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project_4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz',
+        'USER': 'quizuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
